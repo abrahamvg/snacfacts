120a49e18afe3ba036d074ee97814bc5c820b5ca
abrahamvg <abrageor06@gmail.com>
2024-06-24 01:47:32 +0530

    updated the data

--- components/NutritionInfoTable.tsx
+++ components/NutritionInfoTable.tsx
@@ -53,8 +53,51 @@ const data = {
       unit: "mg",
       recommendedValue: 300,
     },
+    {
+      label: "Carbohydrates",
+      value: "carbohydrates",
+      unit: "g",
+      recommendedValue: 300,
+    },
+    {
+      label: "Trans Fat",
+      value: "transFat",
+      unit: "g",
+      recommendedValue: 2,
+    },
+    {
+      label: "Saturated Fat",
+      value: "saturatedFat",
+      unit: "g",
+      recommendedValue: 20,
+    },
+    {
+      label: "Sodium",
+      value: "sodium",
+      unit: "mg",
+      recommendedValue: 2300,
+    },
+    {
+      label: "Iron",
+      value: "iron",
+      unit: "mg",
+      recommendedValue: 18,
+    },
+    {
+      label: "Calcium",
+      value: "calcium",
+      unit: "mg",
+      recommendedValue: 1000,
+    },
+    {
+      label: "Dietary Fiber",
+      value: "dietaryFiber",
+      unit: "g",
+      recommendedValue: 25,
+    },
   ],
 };
+
 
 export default function NutritionInfoTable({
   open,
