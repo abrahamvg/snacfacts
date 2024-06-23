7abdf6d91ff47df5bede4277ce7b67714dd015ca
abrahamvg <abrageor06@gmail.com>
2024-06-24 01:49:39 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import {Copernicus} from '@/fonts/fonts';
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
