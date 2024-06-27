64079980891f82379470ede2f89eeb9dce78c675
abrahamvg <abrageor06@gmail.com>
2024-06-28 00:24:00 +0530

    small commit

--- components/NutritionInfoTable.tsx
+++ components/NutritionInfoTable.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Table,
   TableBody,
@@ -12,8 +14,9 @@ import AddNutritionDropDown from "./AddNutritionDropDown";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import { useNutrition } from "@/hooks/useNutritionHook";
-import {nutrientsData} from "@/providers/constants"
-import { Label } from "@radix-ui/react-select";
+import { nutrientsData } from "@/providers/constants";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 export default function NutritionInfoTable({
   open,
@@ -23,6 +26,8 @@ export default function NutritionInfoTable({
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   const { nutritionalData, setNutritionalData } = useNutrition();
+  const [serveSize, setServeSize] = useState(0);
+  const [weight, setWeight] = useState(0);
 
   const changeTableData = (label: string, newValue: number) => {
     setNutritionalData((prevTableData) =>
@@ -41,47 +46,61 @@ export default function NutritionInfoTable({
         </div>
       </div>
       <div className="content mt-4">
-        <div className="h-64 overflow-y-auto masked-overflow">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-3/4">Nutrition</TableHead>
-              <TableHead className="w-1/4 text-right">Amount</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {nutritionalData.map((nutrient) => {
-              return (
-                <TableRow key={nutrient.nutrientInfo.value}>
-                  <TableCell className="w-3/4">
-                    {nutrient.nutrientInfo.label +
-                      " (" +
-                      nutrient.nutrientInfo.unit +
-                      ")"}
-                  </TableCell>
-                  <TableCell className="w-1/4 text-right">
-                    <Input
-                      type="number"
-                      value={nutrient.value}
-                      placeholder="Enter weight"
-                      className="placeholder:text-neutral-950 px-2 text-right"
-                      onChange={(e) =>
-                        changeTableData(
-                          nutrient.nutrientInfo.label,
-                          Number(e.target.value)
-                        )
-                      }
-                    />
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+        <div className="max-h-64 overflow-y-auto masked-overflow">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-3/4">Nutrition</TableHead>
+                <TableHead className="w-1/4 text-right">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {nutritionalData.map((nutrient) => {
+                return (
+                  <TableRow key={nutrient.nutrientInfo.value}>
+                    <TableCell className="w-3/4">
+                      {nutrient.nutrientInfo.label +
+                        " (" +
+                        nutrient.nutrientInfo.unit +
+                        ")"}
+                    </TableCell>
+                    <TableCell className="w-1/4 text-right">
+                      <Input
+                        type="number"
+                        value={nutrient.value}
+                        placeholder="Enter weight"
+                        className="placeholder:text-neutral-950 px-2 text-right"
+                        onChange={(e) =>
+                          changeTableData(
+                            nutrient.nutrientInfo.label,
+                            Number(e.target.value)
+                          )
+                        }
+                      />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
         </div>
-        <div>
-          <Label></Label>
-        <Input id="servingSize" type="text" placeholder="100g" className="w-20 text-right mt-4" />
+        <div className="flex flex-row w-full items-center mt-4 gap-2 justify-end">
+          <Label htmlFor="servingSize"> per (g): </Label>
+          <Input
+            id="servingSize"
+            type="number"
+            placeholder="100g"
+            className="w-20 text-right"
+          />
+        </div>
+        <div className="flex flex-row w-full items-center mt-4 gap-2 justify-end">
+          <Label htmlFor="totalSize"> Weight (g): </Label>
+          <Input
+            id="totalSize"
+            type="number"
+            placeholder="200g"
+            className="w-20 text-right"
+          />
         </div>
         <Button
           className="w-full mt-4 bg-transparent text-primary border-primary border-2 hover:bg-primary hover:text-neutral-50"
