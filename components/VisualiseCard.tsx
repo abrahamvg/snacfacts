6d4b8bdaec5ad80600dc3f1141a4f0acf33b106f
abrahamvg <abrageor06@gmail.com>
2024-06-29 17:57:35 +0530

    small reoranisation of files

--- components/VisualiseCard.tsx
+++ components/VisualiseCard.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import clsx from "clsx";
-import { comparisonData } from "@/providers/constants";
+import { comparisonData } from "@/lib/constants";
 import {
   Select,
   SelectContent,
